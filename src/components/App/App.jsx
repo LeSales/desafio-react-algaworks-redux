891f6a91e1787753079a1e1b32bf28448cd9cf39
Leandro Sales <leandrossales.96@gmail.com>
2020-10-07 08:40:06 -0300

    add products reducer

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,40 +4,22 @@ import AppHeader from '../AppHeader'
 import { Container, Wrapper } from "./App.styles"
 import LineChart from '../../shared/LineChart'
 import ShoppingList from '../ShoppingList'
-import productsMock from '../../mocks/productsList.json'
 import extractPercentage from '../../utils/extractPercentage'
 import Calculator from '../Calculator'
+import { toggleProduct } from '../../store/Products/Products.actions'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectAllProducts, selectSelectedProducts, selectSelectedProductsTotalPrice } from '../../store/Products/Products.selectors'
 
 function App () {
     const colors = ['#62CBC6', '#00ABAD', '#00858C', '#006073', '#004D61'];
+    const dispatch = useDispatch()
 
-    const [products, setProducts] = useState(productsMock.products);
-    const [selectedProducts, setSelectedProducts] = useState([]);
-    const [totalPrice, setTotalPrice] = useState(0);
-    
-    useEffect(() => {
-        const newSelectedProducts = products
-        .filter(product => product.checked);
+    const products = useSelector(selectAllProducts)
+    const selectedProducts = useSelector(selectSelectedProducts)
+    const totalPrice = useSelector(selectSelectedProductsTotalPrice)
 
-        setSelectedProducts(newSelectedProducts)
-    }, [products]);
-
-    useEffect(() => {
-        const total = selectedProducts
-            .map(product => product.price)
-            .reduce((a, b) => a + b, 0)
-
-        setTotalPrice(total);
-
-    }, [selectedProducts])
-
-    function handleToggle (id, checked, name) {
-        const newProducts = products.map(product => 
-            product.id === id
-                ? { ...product, checked: !product.checked }
-                : product    
-        )
-        setProducts(newProducts)
+    function handleToggle (id) {
+        dispatch(toggleProduct(id))
     }
 
     return <Wrapper>
--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import Checkbox from '../../shared/Checkbox';
-import { Wrapper, Title, Array } from './ShoppingList.styles'
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import Checkbox from "../../shared/Checkbox";
+import { selectAllProducts } from "../../store/Products/Products.selectors";
+import { Wrapper, Title, Array } from "./ShoppingList.styles";
 
-function ShoppingList ({ title, products, onToggle }) {
-    return <Wrapper>
-        <Title>
-            { title }:
-        </Title>
-        <Array>
-            {
-                products.map(product => 
-                    <Checkbox
-                        key={product.id} 
-                        value={product.checked}
-                        title={product.name}
-                        onClick={() => onToggle(product.id, product.checked, product.name)}
-                    />)
-            }
-        </Array>
+function ShoppingList({ title, products, onToggle }) {
+  const productsFromRedux = useSelector(selectAllProducts);
+
+  useEffect(() => console.table(productsFromRedux), [productsFromRedux]);
+
+  return (
+    <Wrapper>
+      <Title>{title}:</Title>
+      <Array>
+        {products.map((product) => (
+          <Checkbox
+            key={product.id}
+            value={product.checked}
+            title={product.name}
+            onClick={() => onToggle(product.id, product.checked, product.name)}
+          />
+        ))}
+      </Array>
     </Wrapper>
+  );
 }
 
 export default ShoppingList;
